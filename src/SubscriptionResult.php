6710d5e9acdae2ca9c96cc4735dc9419432350e6
George Steel <george@net-glue.co.uk>
2021-07-11 12:40:50 +0100

    Documents possible values and turns binary integers into plain integers for readability

--- src/SubscriptionResult.php
+++ src/SubscriptionResult.php
@@ -9,11 +9,35 @@ namespace GSteel\Listless;
  */
 interface SubscriptionResult
 {
-    public const SUBSCRIBED = 0b00001;
-    public const SUBMITTED  = 0b00010;
-    public const DUPLICATE  = 0b00100;
-    public const REJECTED   = 0b01000;
-    public const ERROR      = 0b10000;
+    /**
+     * The subscription was immediately processed and the address was successfully subscribed
+     */
+    public const SUBSCRIBED = 1;
+
+    /**
+     * The subscription was submitted for processing, but the final result is not yet known
+     */
+    public const SUBMITTED = 2;
+
+    /**
+     * The email address submitted is already subscribed
+     */
+    public const DUPLICATE = 4;
+
+    /**
+     * The given email address was rejected during processing and was not subscribed
+     */
+    public const REJECTED = 8;
+
+    /**
+     * Something went wrong during processing and the email address was not subscribed
+     */
+    public const ERROR = 16;
+
+    /**
+     * The email address was successfully processed and accepted, but requires further confirmation
+     */
+    public const PENDING = 32;
 
     /**
      * Whether the outcome of the process is considered successful or not
