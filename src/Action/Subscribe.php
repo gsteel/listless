8d9267cbe40aee322e919b20b3a1ff666f3cd85c
George Steel <george@net-glue.co.uk>
2021-07-11 12:17:12 +0100

    Add some documentation to the subscribe action

--- src/Action/Subscribe.php
+++ src/Action/Subscribe.php
@@ -5,12 +5,22 @@ declare(strict_types=1);
 namespace GSteel\Listless\Action;
 
 use GSteel\Listless\EmailAddress;
+use GSteel\Listless\Exception\Exception;
 use GSteel\Listless\ListId;
 use GSteel\Listless\SubscriberInformation;
 use GSteel\Listless\SubscriptionResult;
 
 interface Subscribe
 {
+    /**
+     * Subscribe a single email address to the given list along with optional information about the subscriber
+     *
+     * Implementors should throw exceptions that implement {@link Exception} if anything goes wrong.
+     *
+     * It should not be exceptional to subscribe an already subscribed address.
+     *
+     * @throws Exception
+     */
     public function subscribe(
         EmailAddress $address,
         ListId $listId,
